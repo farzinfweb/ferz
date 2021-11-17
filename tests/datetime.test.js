32926263694f645750ea17d2b44dee7ea7b8c986
Farzin Farzanehnia <farzin.farzanehnia@gmail.com>
2021-11-17 19:37:26 +0330

    added initial tests

--- tests/datetime.test.js
+++ tests/datetime.test.js
@@ -1,9 +1,19 @@
-import DateTime from '../src/DateTime';
-import PersianCalendar from '../src/PersianCalendar';
+import { DateTime } from '../src/DateTime';
+import { PersianCalendar } from '../src/PersianCalendar';
 
 describe('DateTime', () => {
 	it('should set the calendar passed', () => {
 		const date = new DateTime({}, new PersianCalendar);
 		expect(date.calendar.name).toBe('persian');
 	});
+
+	it('should set the initial date config correctly', () => {
+		const date = new DateTime({year: 1399, month: 10, day: 25}, new PersianCalendar);
+		expect([date.year, date.month, date.day]).toStrictEqual([1399, 10, 25]);
+	});
+
+	it('should set the default calendar', () => {
+		DateTime.setDefaultCalendar(new PersianCalendar);
+		expect(new DateTime({}).calendar.name).toBe('persian');
+	})
 });
